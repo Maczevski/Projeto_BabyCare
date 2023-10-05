2505e2ac64f101cca2e8e757d9c70745560f1090
ismycris <cristielythainara@gmail.com>
2023-10-04 21:28:12 -0300

    atualização

--- Prox partos/assents/javascript/parto.js
+++ Prox partos/assents/javascript/parto.js
@@ -1,11 +1,3 @@
-// Selecione todas as caixas de seleção com a classe 'birth-type-select'
-const birthTypeSelects = document.querySelectorAll('.birth-type-select');
-
-// Adicione um evento de mudança a cada caixa de seleção
-birthTypeSelects.forEach((select, index) => {
-    select.addEventListener('change', function () {
-        // Faça algo quando uma caixa de seleção for alterada, por exemplo:
-        console.log(`Grávida ${index + 1} selecionou: ${this.value}`);
-    });
-});
-
+function selectClass(){
+alert(document.getElementById("01").className)
+}
