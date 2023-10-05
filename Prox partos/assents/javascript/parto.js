b72fcbcb29f144312018e3057a7cdc082d05ed39
ismycris <cristielythainara@gmail.com>
2023-10-05 16:13:48 -0300

    atualização

--- Prox partos/assents/javascript/parto.js
+++ Prox partos/assents/javascript/parto.js
@@ -1,3 +1,14 @@
-function selectClass(){
-alert(document.getElementById("01").className)
+//function selectClass(){
+//alert(document.getElementById("01").className)
+//}
+
+function changeColor(selectElement) {
+    const selectedOption = selectElement.options[selectElement.selectedIndex];
+    const riskSelect = selectElement;
+
+    // Remover todas as classes de cores
+    riskSelect.classList.remove('green', 'red', 'yellow');
+
+    // Adicionar a classe de cor correspondente à opção selecionada
+    riskSelect.classList.add(selectedOption.value);
 }
